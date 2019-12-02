335df937251e697d9e07378cc528efa93b39c80e
Juan de Dios Hernández Dávila <juand_10193@hotmail.com>
2019-12-01 23:29:11 -0600

    Creacion de POST Y PUT con mongo

--- server/server.js
+++ server/server.js
@@ -1,41 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('./config/config')
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario');
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
-    let persona = req.body;
-    if(persona.nombre ===undefined){
-        res.status(400).json({
-            ok:false,
-            mensaje: 'El nombre es necesario',
-            code: 400
-        })
-    }else{
-        res.json({
-            persona: persona
-        })
-    }
-})
 
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario');
-})
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res)=>{
+    if(err) throw err;
+    console.log('Base de datos online')
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`escuchando puerto ${process.env.PORT}`)
